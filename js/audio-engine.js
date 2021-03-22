92bc4f8c1b17c220139b79240e376da9a1dd06b6
moon-clicker <81172964+moon-clicker@users.noreply.github.com>
2021-03-22 09:16:45 -0700

    Update audio-engine.js

--- js/audio-engine.js
+++ js/audio-engine.js
@@ -1,1 +1,62 @@
-var antAudio=function(){"use strict";var i={},o=0,a=function(i,a){var e=new Audio;return e.preload=!0,e.src=a,e.load(),e.onloadeddata=function(i){o++},e.setAttribute("id",i),e},e=function(){return o===Object.keys(i).length};return{init:function(o){!function(o){for(var e in o)if(o.hasOwnProperty(e)){var l=o[e].url,u=o[e].name;if("multiple"===o[e].playMode)for(var r=0;r<999;r++){var t=u+r;i[t]=a(t,l)}else i[u]=a(u,l)}}(o)},play:function(o){var a=e();if(audioFiles[o]&&a)if("multiple"===audioFiles[o].playMode){var l=o+audioFiles[o].currentPlayNum;console.log("play: ",l),i[l].play(),function(i){var o=audioFiles[i].currentPlayNum;audioFiles[i].currentPlayNum=o<999998?o+1:0}(o)}else i[o].play();else console.log("could not play audio, either audio doesn't exist or still waiting for audio files to preload")},stop:function(o){var a=e();audioFiles[o]&&a&&i[o].pause()}}}(),audioFiles={purchase:{name:"purchase",url:"https://github.com/moon-clicker/moon-clicker.github.io/blob/main/audio/purchase.wav?raw=true",playMode:"multiple",currentPlayNum:0},click:{name:"click",url:"https://github.com/moon-clicker/moon-clicker.github.io/blob/main/audio/click.wav?raw=true",playMode:"multiple",currentPlayNum:0}};antAudio.init(audioFiles);
+var antAudio = function() {
+        "use strict";
+        var i = {},
+            o = 0,
+            a = function(i, a) {
+                var e = new Audio;
+                return e.preload = !0, e.src = a, e.load(), e.onloadeddata = function(i) {
+                    o++
+                }, e.setAttribute("id", i), e
+            },
+            e = function() {
+                return o === Object.keys(i).length
+            };
+        return {
+            init: function(o) {
+                ! function(o) {
+                    for (var e in o)
+                        if (o.hasOwnProperty(e)) {
+                            var l = o[e].url,
+                                u = o[e].name;
+                            if ("multiple" === o[e].playMode)
+                                for (var r = 0; r < 999; r++) {
+                                    var t = u + r;
+                                    i[t] = a(t, l)
+                                } else i[u] = a(u, l)
+                        }
+                }(o)
+            },
+            play: function(o) {
+                var a = e();
+                if (audioFiles[o] && a)
+                    if ("multiple" === audioFiles[o].playMode) {
+                        var l = o + audioFiles[o].currentPlayNum;
+                        console.log("play: ", l), i[l].play(),
+                            function(i) {
+                                var o = audioFiles[i].currentPlayNum;
+                                audioFiles[i].currentPlayNum = o < 999998 ? o + 1 : 0
+                            }(o)
+                    } else i[o].play();
+                else console.log("could not play audio, either audio doesn't exist or still waiting for audio files to preload")
+            },
+            stop: function(o) {
+                var a = e();
+                audioFiles[o] && a && i[o].pause()
+            }
+        }
+    }(),
+    audioFiles = {
+        purchase: {
+            name: "purchase",
+            url: "https://moon-clicker.github.io/audio/purchase.wav",
+            playMode: "multiple",
+            currentPlayNum: 0
+        },
+        click: {
+            name: "click",
+            url: "https://moon-clicker.github.io/audio/click.wav",
+            playMode: "multiple",
+            currentPlayNum: 0
+        }
+    };
+antAudio.init(audioFiles);
